7056225e43d7a3f1cd6f2edbd658613b9eb50a17
dongyule <1013048455@qq.com>
2015-05-20 17:25:03 +0800

    bug

---
+++ web/templates/welcome.php
@@ -0,0 +1,17 @@
+<?php include $TEMPLATES_PATH.'/sub/header.php'; ?>
+
+    <div class="wrapper row-offcanvas row-offcanvas-left">
+        <?php include $TEMPLATES_PATH.'/sub/left.php'; ?>
+        <!-- Right side column. Contains the navbar and content of the page -->
+        <aside class="right-side">
+
+            <?php include $TEMPLATES_PATH.'/sub/breadcrumb.php' ?>
+            <!-- Main content -->
+            <section class="content">
+
+
+            </section><!-- /.content -->
+        </aside><!-- /.right-side -->
+    </div><!-- ./wrapper -->
+
+<?php include $TEMPLATES_PATH.'/sub/footer.php'; ?>
